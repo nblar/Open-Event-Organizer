a2e01765b8d36853f65c30b59470a291933b7c5c
Aditya Abhishek <encarcio95@gmail.com>
2021-06-26 11:52:01 +0530

    changes made 2

--- demo/src/main/resources/static/createEvent.js
+++ demo/src/main/resources/static/createEvent.js
@@ -1,62 +1,63 @@
-var img=[];
+var img = [];
 
 function printDate() {
-    var elem=document.getElementById('startTime').value;
+    var elem = document.getElementById('startTime').value;
     console.log(elem);
 
-    var email= document.getElementById("email").value;
-    var title= document.getElementById("title").value;
-    var description= document.getElementById("description").value;
-    var category= document.getElementById("category").value;
-    var event_type=document.getElementById("event_type").value;
-    var price=document.getElementById("price").value;
-    var venue=document.getElementById("venue").value;
-    var startTime=document.getElementById("startTime").value;
-    var endTime=document.getElementById("endTime").value;
-    var startTime=document.getElementById("startTime").value;
+    var email = document.getElementById("email").value;
+    var title = document.getElementById("title").value;
+    var description = document.getElementById("description").value;
+    var category = document.getElementById("category").value;
+    var event_type = document.getElementById("event_type").value;
+    var price = document.getElementById("price").value;
+    var venue = document.getElementById("venue").value;
+    var startTime = document.getElementById("startTime").value;
+    var endTime = document.getElementById("endTime").value;
+    var startTime = document.getElementById("startTime").value;
 
 
-    var dateTime=document.getElementById('startTime').value;
-    var splittedDateTime=dateTime.split('T');
-    var startDate=splittedDateTime[0];
-    var startTime=splittedDateTime[1];
-    dateTime=document.getElementById('endTime').value;
-    splittedDateTime=dateTime.split('T');
-    var endDate=splittedDateTime[0];
-    var endTime=splittedDateTime[1];
-    
-    var img1=img[0];
-    
-    if(img.length==2)
-    var img2=img[1];
-    
+    var dateTime = document.getElementById('startTime').value;
+    var splittedDateTime = dateTime.split('T');
+    var startDate = splittedDateTime[0];
+    var startTime = splittedDateTime[1];
+    dateTime = document.getElementById('endTime').value;
+    splittedDateTime = dateTime.split('T');
+    var endDate = splittedDateTime[0];
+    var endTime = splittedDateTime[1];
+
+    var img1 = img[0];
+
+    if (img.length == 2)
+        var img2 = img[1];
+
     else
-    var img2=null;
+        var img2 = null;
 
 
-    var data = JSON.stringify({"eventname":title,"eventtype":event_type,"category":category,"venue":venue,
-    "starttime":startTime,"endtime":endTime,"fee":price,"email":email,"description":description,
-    "date":startDate,"endDate":endDate, "imgUrl1":img1, "imgUrl2": img2});
+    var data = JSON.stringify({
+        "eventname": title, "eventtype": event_type, "category": category, "venue": venue,
+        "starttime": startTime, "endtime": endTime, "fee": price, "email": email, "description": description,
+        "date": startDate, "endDate": endDate, "imgUrl1": img1, "imgUrl2": img2
+    });
 
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
 
-    xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-        var status_code=this.status;
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            var status_code = this.status;
 
-        if(status_code==202){
-            console.log("Success");
-            details_fetch();
-           window.location.replace("index.html");
+            if (status_code == 202) {
+                console.log("Success");
+                details_fetch();
+                window.location.replace("index.html");
             }
-        else
-        {
-            alert("Wrong email");
-            location.reload();
+            else {
+                alert("Wrong email");
+                location.reload();
+            }
+
         }
-        
-    }
     });
 
     xhr.open("POST", "http://localhost:8080/eventRegister");
@@ -64,39 +65,44 @@ function printDate() {
     xhr.setRequestHeader("Content-Type", "application/json");
 
     xhr.send(data);
+
+}
+function displayEventDetails(cardID) {
+    window.location.href="eventDisplay.html";
+    var event_Title=document.getElementById("event-title");
+    event_Title.innerHTML="Pull from here";
     
 }
-
 function details_fetch() {
-    
-    var data="";
+
+    var data = "";
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
 
-    xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-        let elem=this.responseText;
-        let obj= JSON.parse(elem);
-        console.log(obj);
-
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            let elem = this.responseText;
+            let obj = JSON.parse(elem);
+            console.log(obj);
+            var card_id=1;
             var display = document.getElementById('id');
             Object.keys(obj).forEach(function (key) {
-            let details = obj[key];
-            console.log(obj[key]);
-            let eventID = details[0];
-            let title = details[1];
-            let eventType = details[3];
-            let category = details[4];
-            let venue = details[5];
-            let time = details[6];
-            let date = details[9];
-            let description = details[11];
-            let img1= details[12];
-            var card = `<div class="card"><a href="javascript:void(0)" onclick="getCardDetails(this)><img src=${img1} class="card-img-top" width="150px" alt="..."></a><div class="card-body"><h5 class="card-title">${title}</h5><p class="card-text">${date} ${time}</p><p class="card-text">${description}</p><p class="card-text"><small class="text-muted">By Coding Club</small></p></div></div>`;
-            display.innerHTML += card;
-        });
-        
-    }
+                let details = obj[key];
+                console.log(obj[key]);
+                let eventID = details[0];
+                let title = details[1];
+                let eventType = details[3];
+                let category = details[4];
+                let venue = details[5];
+                let time = details[6];
+                let date = details[9];
+                let description = details[11];
+                let img1 = details[12];
+                var card = `<div id=${eventID} class="card"><a href="javascript:dislayEventDetails(${eventID})"><img src=${img1} class="card-img-top" width="150px" alt="..."></a><div class="card-body"><h5 class="card-title">${title}</h5><p class="card-text">${date} ${time}</p><p class="card-text">${description}</p><p class="card-text"><small class="text-muted">By Coding Club</small></p></div></div>`;
+                display.innerHTML += card;
+            });
+
+        }
     });
 
     xhr.open("GET", "http://localhost:8080/returnDetails");
@@ -108,22 +114,23 @@ function details_fetch() {
 }
 
 
-function UploadImage(){
-	var myWidget = cloudinary.createUploadWidget({
-		  cloudName: 'addendum', 
-		  uploadPreset: 'jzywfmyl'}, (error, result) => { 
-		    if (!error && result && result.event === "success") { 
-		    //  console.log('*************************Done! Here is the image URLLLLLLLLL: ', result.info.secure_url); 
-		    	img.push(result.info.secure_url);
-		    	
-		    	 console.log('*************************Done! Here is the image URLLLLLLLLL: ', img); 
-		    }
-		
-		    console.log(result);
-		  }
-		)
-		
-		document.getElementById("upload_widget").addEventListener("click", function(){
-		    myWidget.open();
-		  }, false);
+function UploadImage() {
+    var myWidget = cloudinary.createUploadWidget({
+        cloudName: 'addendum',
+        uploadPreset: 'jzywfmyl'
+    }, (error, result) => {
+        if (!error && result && result.event === "success") {
+            //  console.log('*************************Done! Here is the image URLLLLLLLLL: ', result.info.secure_url); 
+            img.push(result.info.secure_url);
+
+            console.log('*************************Done! Here is the image URLLLLLLLLL: ', img);
+        }
+
+        console.log(result);
+    }
+    )
+
+    document.getElementById("upload_widget").addEventListener("click", function () {
+        myWidget.open();
+    }, false);
 }
