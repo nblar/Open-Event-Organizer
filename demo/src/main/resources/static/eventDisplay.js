3b8535c00357a121d75574e96ddc6bb5926bf349
Aditya Abhishek <encarcio95@gmail.com>
2021-06-26 12:32:16 +0530

    changes made 3

--- demo/src/main/resources/static/eventDisplay.js
+++ demo/src/main/resources/static/eventDisplay.js
@@ -1,7 +1,12 @@
-async function displayEventDetails(cardID) {
-    window.location.href="eventDisplay.html";
-    var event_Title=await document.getElementById("event-title");
+var card_ID;
+function displayEventDetails(cardID) {
+    window.location.replace("eventDisplay.html");
+    card_ID=cardID;
+}
+function putEventDetails(card_ID){
+    var event_Title=document.getElementById("event-title");
     console.log(cardID);
-    event_Title.innerHTML="Pull from here";
+    event_Title.innerHTML=cardID.querySelector(".card-title").innerHTML;
     
 }
+document.addEventListener("DOMContentLoaded",putEventDetails,false);
