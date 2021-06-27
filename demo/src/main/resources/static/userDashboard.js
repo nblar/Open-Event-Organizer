95a4de2c0d110ccf50b59fc7b0b572ef292f2a3b
Aditya Abhishek <encarcio95@gmail.com>
2021-06-27 11:36:04 +0530

    added userdashboard card

--- demo/src/main/resources/static/userDashboard.js
+++ demo/src/main/resources/static/userDashboard.js
@@ -18,7 +18,7 @@ function dashDetails() {
                     console.log(obj[key]);
                     username.innerHTML = details[14];
                     var card = `  <div class="row">
-                    <div class="card">
+                    <div class="card" id="${details[0]}">
                         <div class="row">
                             <div class="col-3">
                                 <img src="${details[12]}" class="card-img" alt="...">
@@ -29,6 +29,9 @@ function dashDetails() {
                                     <p class="card-text">${details[11]}</p>
                                     <a href="eventDisplay.html?eventType=${details[3]}&id=${details[0]}&title=${details[1]}&img=${details[12]}&startTime=${details[6]}&endTime=${details[7]}&startDate=${details[9]}&endDate=${details[10]}&description=${details[11]}&category=${details[4]}&venue=${details[5]}" class="btn btn-primary">More details</a>
                                 </div>
+                            </div>
+                            <div class="col-3">
+                                <button type="button" onclick="deleteEvent(${details[14]},${details[0]})" class="btn btn-block btn-attend-event btn-outline-danger">Delete Event</button>
                             </div>
                         </div>
                     </div>
@@ -47,3 +50,21 @@ function dashDetails() {
     xhr.send(data);
 }
 
+function deleteEvent(userEmail,eventID) {
+    var data = JSON.stringify({ "email": userEmail,"eventID":eventID });
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            var status_code = this.status;
+            if (status_code == 200) {
+                console.log("event deleted successfully");
+                document.getElementById(""+eventID).remove;
+            }
+        }
+    });
+    xhr.open("POST", "http://localhost:8080/deleteEvent");
+    xhr.setRequestHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmb28iLCJleHAiOjE1OTg1ODY4OTQsImlhdCI6MTU5ODU1MDg5NH0.rnwwXGxDN5z3Y7Cz0z_MeCwpUJ0RLbVvYce5xYWMwd8");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+}
